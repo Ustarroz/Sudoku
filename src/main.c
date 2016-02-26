c974db84a4081c0f5ecc5caf6f52398612126ac8
ustarr_r <ustarr_r@epitech.eu>
2016-02-26 21:22:58 +0100

    getnextline

--- src/main.c
+++ src/main.c
@@ -1,18 +1,22 @@
 /*
 ** main.c for sudoki-bi in /home/puilla_e/Semestre2/progelem/rendu/
-** 
+**
 ** Made by edouard puillandre
 ** Login   <puilla_e@epitech.net>
-** 
+**
 ** Started on  Fri Feb 26 18:57:29 2016 edouard puillandre
-** Last update Fri Feb 26 18:58:17 2016 edouard puillandre
+** Last update Fri Feb 26 21:21:57 2016 Voyevoda
 */
 
 #include "sudoki.h"
 
-int	main(int argc, char **argv)
+int	main(int ac, char **av)
 {
-  (void) argc;
-  (void) argv;
+  if (ac == 2)
+    {
+
+    }
+  else
+    fprintf(stderr, "%s\n", ARG_ERG_MSG);
   return (0);
 }
