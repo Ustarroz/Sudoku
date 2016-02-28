be69ef6583b1cf6e3a664350e765536eda6e6690
ustarr_r <ustarr_r@epitech.eu>
2016-02-28 15:34:03 +0100

    init variant

--- bonus/src/main.c
+++ bonus/src/main.c
@@ -5,7 +5,7 @@
 ** Login   <puilla_e@epitech.net>
 **
 ** Started on  Fri Feb 26 18:57:29 2016 edouard puillandre
-** Last update Sun Feb 28 15:23:25 2016 Voyevoda
+** Last update Sun Feb 28 15:32:57 2016 Voyevoda
 */
 
 #include "sudoki.h"
@@ -18,7 +18,7 @@ int	check_av(char *str)
 
   len = strlen(str);
   if (len != SIZE_DOUBLE || len != SIZE_TRIPLE ||
-      len != SIZE_QUADRA)
+      len != SIZE_QUADRA || len != SIZE_PENTA)
     return (1);
   i = - 1;
   while (str[++i] != '\0')
@@ -35,16 +35,28 @@ int	check_av(char *str)
   return (0);
 }
 
+t_variant	*init_variant()
+{
+  t_variant	*alpha;
+
+  if ((alpha = malloc(sizeof(t_variant))) == NULL)
+    return (NULL);
+  return (alpha);
+}
+
 int		main(int ac, char **av)
 {
   t_sudo	*sudo;
+  t_variant	*alphabet;
 
+  if ((alphabet = init_variant()) == NULL)
+    return (1);
   if (ac != 2 || check_av(av[1]) == 1)
     {
       fprintf(stderr, ARG_ERR_MSG);
       return (1);
     }
-  if ((sudo = my_init_sudo()) == NULL)
+  if ((sudo = my_init_sudo(alphabet)) == NULL)
     return (1);
   my_resolve_all(sudo);
   my_print_sudo(sudo);
