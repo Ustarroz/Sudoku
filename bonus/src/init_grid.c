66070c6f2deef6abb2f794ecd4f74404837c28a8
ustarr_r <ustarr_r@epitech.eu>
2016-02-28 16:19:40 +0100

    maj init grid

--- bonus/src/init_grid.c
+++ bonus/src/init_grid.c
@@ -5,7 +5,7 @@
 ** Login   <voyevoda@epitech.net>
 **
 ** Started on  Fri Feb 26 21:37:53 2016 Voyevoda
-** Last update Sun Feb 28 15:17:23 2016 Voyevoda
+** Last update Sun Feb 28 16:18:44 2016 Voyevoda
 */
 
 #include "sudoki.h"
@@ -13,38 +13,28 @@
 
  int	check_char(char *str, t_variant *alphabet, int i)
  {
-   int	j;
+  }
 
-   j = -1;
-   while (alphabet->base[++j] != '\0')
-     {
-       if (alphabet->base[j] == str[i])
-	 return (0);
-     }
-   fprintf(stderr, MAP_ERR_MSG);
-   return (-1);
- }
-
-int	my_check_line(char *str)
+int	my_check_line(char *str, t_variant *alphabet)
 {
   int	i;
   int	space;
 
   i = 0;
   space = 1;
-  if (str[0] != '|' || str[LINE] != '|')
+  if (str[0] != '|' || str[alphabet->line] != '|')
     {
       fprintf(stderr, MAP_ERR_MSG);
       return (-1);
     }
-  while (++i != LINE)
+  while (++i != alphabet->size)
     {
       if (space == 1 && str[i] != ' ')
 	{
 	  fprintf(stderr, MAP_ERR_MSG);
 	  return (-1);
 	}
-      else if (space == 0 && (check_char == -1))
+      else if (space == 0 && (str_to_int(str, alphabet, i) == -1))
 	{
 	  fprintf(stderr, MAP_ERR_MSG);
 	  return (-1);
@@ -54,24 +44,34 @@ int	my_check_line(char *str)
   return (0);
 }
 
-void	str_to_int(char *s, int *line)
+int	str_to_int(char *s, int *line, t_variant *alphabet)
 {
   int	i;
   int	j;
+  int	k;
 
   j = -1;
   i = 2;
-  while (++j != SIZE)
+  k = -1;
+  while (alphabet->base[++k] != '\0')
     {
-      if (s[i] == ' ')
-	line[j] = 0;
-      else
-	line[j] =  48 - s[i];
-      i = i + 2;
+      if (alphabet->base[k] == str[i])
+	while (++j != alphabet->size)
+	  {
+	    if (s[i] == ' ')
+	      line[j] = 0;
+	    else
+	      {
+		line[j] = k + 1;
+		return (0);
+	      }
+	    i = i + 2;
+	  }
     }
+  return (-1);
 }
 
-int	*my_get_line(int fd)
+int	*my_get_line(int fd, t_variant *alphabet)
 {
   char	*s;
   int	*line;
@@ -81,18 +81,17 @@ int	*my_get_line(int fd)
       fprintf(stderr, READ_ERR_MSG);
       return (NULL);
     }
-  if ((line = malloc(sizeof(int) * (SIZE))) == NULL)
+  if ((line = malloc(sizeof(int) * (alphabet->size))) == NULL)
     {
       free(s);
       fprintf(stderr, MAL_ERR_MSG);
       return (NULL);
     }
-  if ((my_check_line(s)) == -1)
+  if ((my_check_line(s, alphabet)) == -1)
     {
       free(s);
       return (NULL);
     }
-  str_to_int(s, line);
   free(s);
   return (line);
 }
@@ -104,15 +103,15 @@ int	**my_init_grid(int fd, t_variant *alphabet)
   int	check;
 
   i = -1;
-  if ((tab = malloc(sizeof(int *) * (SIZE))) == NULL)
+  if ((tab = malloc(sizeof(int *) * (alphabet->size))) == NULL)
     {
       fprintf(stderr, MAL_ERR_MSG);
       return (NULL);
     }
   while (++i < SIZE)
-    if ((tab[i] = my_get_line(fd)) == NULL)
+    if ((tab[i] = my_get_line(fd, alphabet)) == NULL)
       return (NULL);
-  if ((check = check_border(fd)) == - 1)
+  if ((check = check_border(fd, alphabet)) == - 1)
     {
       fprintf(stderr, READ_ERR_MSG);
       return (NULL);
